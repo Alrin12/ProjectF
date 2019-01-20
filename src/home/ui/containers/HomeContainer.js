df54b10f06350fdef2fe049492a57441cec90b48
Ck-Republic <Alrin12@github.com>
2019-01-20 18:17:13 +0900

    Project Init

---
+++ src/home/ui/containers/HomeContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class HomeContainer extends Component<Props> {
+  render() {
+    return(
+      <View>
+        <Text>HomeContainer</Text>
+      </View>
+    )
+  };
+};
